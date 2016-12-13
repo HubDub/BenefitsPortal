ac601488943659ca639681bd4d1b2ec29f169cfe
Debbie Bourne <dblashrink@hotmail.com>
2016-12-13 15:47:50 -0600

    added tests of models

---
+++ src/BenefitsPortal/Controllers/HrController.cs
@@ -0,0 +1,35 @@
+﻿using BenefitsPortal.Data;
+using BenefitsPortal.Models;
+using BenefitsPortal.Models.BenefitsViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BenefitsPortal.Controllers
+{
+    [Authorize]
+    public class HrController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private ApplicationDbContext newContext;
+        public HrController(UserManager<ApplicationUser> userManager, ApplicationDbContext ctx1)
+        {
+            _userManager = userManager;
+            newContext = ctx1;
+        }
+
+        //this method will get the employees from the DB and pass them to the view model
+        public async Task<IActionResult> Dashboard()
+        {
+            AllEmployeesViewModel model = new AllEmployeesViewModel(_userManager, newContext);
+            model.Employees = await newContext.Employee.ToListAsync();
+            return View(model);
+        }
+    }
+}
---
+++ src/BenefitsPortal/Models/BenefitsViewModels/AllEmployeesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BenefitsPortal.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BenefitsPortal.Models.BenefitsViewModels
+{
+    public class AllEmployeesViewModel
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        public AllEmployeesViewModel(UserManager<ApplicationUser> userManager, ApplicationDbContext ctx1) { } 
+    }
+}
